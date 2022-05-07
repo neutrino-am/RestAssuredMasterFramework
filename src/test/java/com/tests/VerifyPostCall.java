e62d9d7372f8726143ea81da000fb9d4733981c4
AshutoshM <connectwithashutoshm@gmail.com>
2022-05-08 00:32:50 +0530

    adds first api post test

--- src/test/java/com/tests/VerifyPostCall.java
+++ src/test/java/com/tests/VerifyPostCall.java
@@ -1,13 +1,26 @@
 package com.tests;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.*;
 
 public class VerifyPostCall {
 
     @Test
     public void test() {
 
-        // https://reqres.in/api/users
+        Response response = given()
+                .header("Content-Type", ContentType.JSON)
+                .log()
+                .all()
+                .body(new File(System.getProperty("user.dir")+ "/src/test/resources/TestData/test.json"))
+                .post("https://reqres.in/api/users");
+
+        response.prettyPrint();
     }
 }
 
