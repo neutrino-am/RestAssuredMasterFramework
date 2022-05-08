60418ee523cecb671acea30dcb4aa92a3513af43
AshutoshM <connectwithashutoshm@gmail.com>
2022-05-08 23:02:58 +0530

    chore: adds assertions

--- src/test/java/com/tests/VerifyPostCall.java
+++ src/test/java/com/tests/VerifyPostCall.java
@@ -1,7 +1,10 @@
 package com.tests;
 
+import com.fasterxml.jackson.databind.jsonschema.JsonSchema;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -21,6 +24,13 @@ public class VerifyPostCall {
                 .post("https://reqres.in/api/users");
 
         response.prettyPrint();
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertEquals(response.getHeader("Content-Type"),"application/json; charset=utf-8");
+        Assert.assertTrue(response.getTime() < 2000,"Response time is less than 2 sec");
+
+        response.then().body(JsonSchemaValidator.matchesJsonSchema( new File (System.getProperty("user.dir")+ "/src/test/resources/schemas/schema.json")));
+
     }
 }
 
